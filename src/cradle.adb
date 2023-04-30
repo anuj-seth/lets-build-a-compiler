057d7faec738a0f0bc61ebb43e59714d61d43177
Anuj Seth <anujseth76@gmail.com>
2023-04-30 20:49:53 +0530

    support debug printing of entry/exit of functions

--- src/cradle.adb
+++ src/cradle.adb
@@ -1,11 +1,42 @@
 with Ada.Text_IO;
 with Ada.Characters.Latin_1;
 with Ada.Characters.Handling;
+with Ada.Environment_Variables;
 
 package body Cradle is
    package TIO renames Ada.Text_IO;
    package CL1 renames Ada.Characters.Latin_1;
    package CH renames Ada.Characters.Handling;
+   package ENV renames Ada.Environment_Variables;
+
+   DEBUG : Boolean;
+   FUNCTION_DEPTH : Natural := 1;
+
+   procedure Enter_Fn (Fn_Name : String) is
+   begin
+      if not DEBUG then
+         return;
+      end if;
+
+      FUNCTION_DEPTH := FUNCTION_DEPTH + 1;
+      for I in 1 .. FUNCTION_DEPTH loop
+         TIO.Put (CL1.Space);
+      end loop;
+      TIO.Put_Line ("->" & Fn_Name);
+   end Enter_Fn;
+
+   procedure Exit_Fn (Fn_Name : String) is
+   begin
+      if not DEBUG then
+         return;
+      end if;
+
+      for I in 1 .. FUNCTION_DEPTH loop
+         TIO.Put (CL1.Space);
+      end loop;
+      TIO.Put_Line ("<-" & Fn_Name);
+      FUNCTION_DEPTH := FUNCTION_DEPTH - 1;
+   end Exit_Fn;
 
    procedure Error (S : String) is
    begin
@@ -43,4 +74,17 @@ package body Cradle is
       TIO.New_Line;
    end Emit_Line;
 
+begin
+   declare
+      Env_Var_Value : constant String
+         := ENV.Value ("DEBUG", "NOT FOUND");
+   begin
+      if Env_Var_Value = "NOT FOUND" then
+         DEBUG := False;
+      elsif Env_Var_Value = "on" then
+         DEBUG := True;
+      else
+         DEBUG := False;
+      end if;
+   end;
 end Cradle;
--- src/cradle.ads
+++ src/cradle.ads
@@ -7,4 +7,6 @@ package Cradle is
    function Is_Digit (X : Character) return Boolean;
    procedure Emit (S : String);
    procedure Emit_Line (S : String);
+   procedure Enter_Fn (Fn_Name : String);
+   procedure Exit_Fn (Fn_Name : String);
 end Cradle;
